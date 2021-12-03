3fbb3cc3d05bec7c92e872e123244d838b0ab768
Matthew Ward <wardm1993@gmail.com>
2021-12-02 23:06:44 -0500

    Installed Semantic-ui React Library
    Made a few simple components

---
+++ te-app/src/components/Button.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import {Button} from 'semantic-ui-react'
+
+const TheButton = () => (
+    <div>
+        <Button content='Primary' primary />
+        <Button content='Secondary' secondary />
+    </div>
+)
+
+export default TheButton
+
---
+++ te-app/src/components/Container.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import {Container} from 'semantic-ui-react'
+
+const TheContainer = () => (
+  <Container>
+    <p>
+      This is a container
+    </p>
+  </Container>
+)
+
+export default TheContainer
