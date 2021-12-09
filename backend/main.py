e00a0e1e2d3afa6eee8f5c1bc14333e52da40841
Matthew Ward <wardm1993@gmail.com>
2021-12-08 21:51:37 -0500

    Error in code

--- backend/main.py
+++ backend/main.py
@@ -19,13 +19,13 @@ app = FastAPI()
 #     "http://localhost:3000",
 # ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
